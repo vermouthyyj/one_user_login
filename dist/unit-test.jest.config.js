5bfe9fc3c2eac2c625774358ef2d571dbc47d37e
vermouthyyj <yanyujun666@gmail.com>
2023-12-01 13:04:24 +1100

    docs:add testing strategy

---
+++ dist/unit-test.jest.config.js
@@ -0,0 +1,11 @@
+"use strict";
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: [
+        "<rootDir>/controller/__test__",
+        "<rootDir>/services/__test__",
+        "<rootDir>/repositories/__test__",
+    ],
+    testMatch: ["**/*.test.ts"],
+};
